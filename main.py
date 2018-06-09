5471d06ad1337a901f4155440c1773a5741fabe3
Raphael Adamski <raphaeladamski@hotmail.com>
2018-06-09 17:27:44 -0300

    Create args, create calc of new dimensions, create calc of original position of pixel for scale and create nearst neighbor interpolation mode

--- main.py
+++ main.py
@@ -1,26 +1,93 @@
 import argparse
 import cv2
 import numpy as np
+import math
 
-modes = ("projection", "hough")
-pre = ("crop", "sobel", "otsu", "contours", "gray")
+def centered_crop(img, new_height, new_width):
+    width = np.size(img, 1)
+    height = np.size(img, 0)
+
+    left = (width - new_width) // 2
+    top = (height - new_height) // 2
+    right = (width + new_width) // 2
+    bottom = (height + new_height) // 2
+    c_img = img[top:bottom, left:right, :]
+    return c_img
+
+
+def nearest_neighbor(img_orig, x, y):
+    return img_orig[round(x)][round(y)]
+
+
+
+modes = ("nearest_neighbor", "bilinear", "bicubic", "lagrange" )
 parser = argparse.ArgumentParser(description='Fix tilted images')
+group1 = parser.add_mutually_exclusive_group(required=True)
+group1.add_argument('-a', '--angle', type=float, help='Rotation angle counter-clockwise in degrees')
+group1.add_argument('-e', '--escala', type=float, help='Scaling factor')
+parser.add_argument('-d', '--dimensions', type=int, nargs=2, help='Pair of values, height and width, of output image',
+                    default=[-1, -1])
+parser.add_argument('-m', '--mode', type=str, help='Interpolation mode',
+                    default=modes[0], choices=modes)
 parser.add_argument('-i', '--input', type=str, help='Input image path', required=True)
 parser.add_argument('-o', '--output', type=str, help='Output image path')
-parser.add_argument('-m', '--mode', type=str, help='Technique for alignment algorithm',
-                    default='projection', choices=modes)
-parser.add_argument('-p', '--pre', type=str, help='Technique for preprocessing',default='gray', choices=pre, nargs="*")
-parser.add_argument('-c', '--crop', type=int, help='Crop window size', default=500)
 
 args = vars(parser.parse_args())
+print(args)
 INPUT = args["input"]
 OUTPUT = args["output"]
 MODE = args["mode"]
-PRE = args["pre"]
-CROP = args["crop"]
+DIM = args["dimensions"]
+SCALE = args["escala"]
+ANGLE = args["angle"]
+
+img_orig = cv2.imread(INPUT)
+img_gray = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
+
+# calculate new dimensions
+width = np.size(img_gray, 1)
+height = np.size(img_gray, 0)
+if ANGLE:
+    # ANGLE become positive and less than 360
+    ANGLE = ANGLE%360
+    # ANGLE between 0 and 180
+    simple_angle = math.fabs(ANGLE % 180 - ANGLE//180*180)
+    if ANGLE > 90:
+        height = width
+        width = height
+        simple_angle = simple_angle - 90
+    new_width = round(width * math.cos(math.radians(simple_angle)) + height * math.sin(math.radians(simple_angle)))
+    new_height = round(width * math.sin(math.radians(simple_angle)) + height * math.cos(math.radians(simple_angle)))
+
+elif SCALE:
+    new_width = round(width * SCALE)
+    new_height = round(height * SCALE)
+
+img_out = np.zeros((new_height,new_width), dtype=img_gray.dtype)
+width = np.size(img_gray, 1)
+height = np.size(img_gray, 0)
+for x in range(new_height):
+    for y in range(new_width):
+        if ANGLE:
+            x_orig = x
+            y_otig = y
+        if SCALE:
+            x_orig = x/SCALE
+            if round(x_orig) == height:
+                x_orig = x_orig-0.5
+            y_orig = y/SCALE
+            if round(y_orig) == width:
+                y_orig = y_orig-0.5
+
+        if MODE == modes[0]:
+            img_out[x][y] = nearest_neighbor(img_gray, x_orig, y_orig)
+
+
+
 
 if OUTPUT and not OUTPUT.endswith(".png"):
     OUTPUT = OUTPUT + ".png"
 
-img_orig = cv2.imread(INPUT)
 cv2.imshow("Original", img_orig)
+cv2.imshow("Transformed", img_out)
+cv2.waitKey(0)
